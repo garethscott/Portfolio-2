74fbd0fefaff22fff4abdda4ef134bdc121c483b
garethscott <59648675+garethscott@users.noreply.github.com>
2020-09-30 18:43:28 +0100

    Futher developments to the project page, project square component

--- src/components/ProjectsPage/ProjectBox.js
+++ src/components/ProjectsPage/ProjectBox.js
@@ -1,17 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const ProjectBox = () => {
+const ProjectBox = (props) => {
     return (
-        <StyledProjectBox>
-
+        <StyledProjectBox to="#">
+            <StyledImg src={props.img} alt={props.alt} />
+            <StyledFig>
+                <StyledImgHeading>{props.heading}</StyledImgHeading>
+                <StyledSubHeading>{props.subHeading}</StyledSubHeading>
+            </StyledFig>
         </StyledProjectBox>
     )
 }
 export default ProjectBox;
 
-const StyledProjectBox = styled.div`
+const StyledProjectBox = styled(Link)`
     width: 87%;
-    height: 200px;
-    background: blue;
+    height: 250px;
+    // background: lightblue;
+    text-decoration: none;
+    margin-bottom: 14px;
+`;
+const StyledImg = styled.img`
+    width: 100%;
+    height:
+`;
+const StyledFig = styled.div`
+    width: 100px;
+`;
+const StyledImgHeading = styled.h3`
+    margin-top: 2px;
+    color: rgb(0, 50, 57);
+    font-family: helvetica;
+    font-size: 15px;
+    width: 150px;
+`;
+const StyledSubHeading = styled.h3`
+    color: rgb(0, 50, 57);
+    font-family: helvetica;
+    font-size: 15px;
+    font-weight: 200;
+    margin-top: -15px;
 `;
--- src/components/ProjectsPage/ProjectsPage.js
+++ src/components/ProjectsPage/ProjectsPage.js
@@ -3,6 +3,12 @@ import styled from 'styled-components';
 import ProjectsHeading from './ProjectsHeading';
 import ProjectsCopy from './ProjectsCopy';
 import ProjectBox from './ProjectBox';
+import KeyCodeImg from './Images/Project-Square-Keycode.jpg';
+import DrumKitImg from './Images/Project-Square-DrumKit.jpg';
+import DiceGameImg from './Images/Project-Square-DiceGame.jpg';
+import CalcImg from './Images/Project-Square-Calc.jpg';
+import AddBookImg from './Images/Project-Square-AddressBook.jpg';
+
 
 const ProjectsPage = () => {
     return (
@@ -13,8 +19,33 @@ const ProjectsPage = () => {
                     <ProjectsCopy />
                 </StyledProjectContent>
             </StyledProjectsIntroBox>
+
             <StyledProjectImageBox>
-                <ProjectBox />
+                <ProjectBox
+                    img={KeyCodeImg}
+                    heading="Key Code"
+                    subHeading="Javascript"
+                />
+                <ProjectBox
+                    img={DrumKitImg}
+                    heading="Drum kit"
+                    subHeading="Javascript"
+                />
+                <ProjectBox
+                    img={DiceGameImg}
+                    heading="Drum kit"
+                    subHeading="Javascript"
+                />
+                <ProjectBox
+                    img={CalcImg}
+                    heading="Calculator"
+                    subHeading="React.js"
+                />
+                <ProjectBox
+                    img={AddBookImg}
+                    heading="Address Book"
+                    subHeading="React.js"
+                />
             </StyledProjectImageBox>
         </>
     )
@@ -39,8 +70,10 @@ const StyledProjectContent = styled.div`
 `;
 const StyledProjectImageBox = styled.div`
     width: 100%;
-    height: 100vh;
-    background: red;
+    height: 1500px;
+    background: rgb(253, 167, 159);
     display: flex;
-    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 0;
 `;
