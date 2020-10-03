8fcf345d05631910f40c7b522914a7428f7ef46c
garethscott <59648675+garethscott@users.noreply.github.com>
2020-10-03 17:23:03 +0100

    Added some feedback to the logo

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import ScrollToTopController from '../ScrollToTopController';
 import GreetingBox from './GreetingBox';
 import AboutBox from './AboutBox'
 import LogoBlack from './images/logo-border-black.png';
+import LogoHover from './images/logo-hover.png';
 
 
 const LandingPage = () => {
+    const [logoHover, setLogoHover] = useState(true);
+
     return (
         <div>
             <ScrollToTopController />
             <Link to="/home">
-                <StyledLogoBlack src={LogoBlack} alt="Gareth's Black Logo" />
+                <div
+                    onMouseEnter={() => setLogoHover(false)}
+                    onMouseLeave={() => setLogoHover(true)}>
+                    {logoHover ? (
+                        <StyledLogoBlack
+                            src={LogoBlack}
+                            alt="Gareth's Black Logo"
+                        />) : (
+                            <StyledLogoPink
+                                src={LogoHover}
+                                alt="Gareth's Pink Logo"
+                            />)}
+                </div>
             </Link>
             <GreetingBox />
             <AboutBox />
-        </div>
+        </div >
     )
 }
 
 export default LandingPage;
 
 const StyledLogoBlack = styled.img`
+    width: 30px;
+    position: fixed;
+    right: 20px;
+    top: 20px;
+`;
+const StyledLogoPink = styled.img`
     width: 30px;
     position: fixed;
     right: 20px;
