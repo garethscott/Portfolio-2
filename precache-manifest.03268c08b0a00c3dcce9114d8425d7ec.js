8511440e46badf76899b0c8ccbd022581aa63c0a
garethscott <59648675+garethscott@users.noreply.github.com>
2020-10-08 17:58:26 +0100

    Updates

---
+++ precache-manifest.03268c08b0a00c3dcce9114d8425d7ec.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc16bc0148bac10b58b7decab756523",
+    "url": "/Portfolio-2/index.html"
+  },
+  {
+    "revision": "312749217a70e5124cb9",
+    "url": "/Portfolio-2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b92472b8d59e090bd671",
+    "url": "/Portfolio-2/static/js/2.682a908a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio-2/static/js/2.682a908a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312749217a70e5124cb9",
+    "url": "/Portfolio-2/static/js/main.028fa555.chunk.js"
+  },
+  {
+    "revision": "d760826089b9bba24b4d",
+    "url": "/Portfolio-2/static/js/runtime-main.c5dc98f4.js"
+  },
+  {
+    "revision": "621f8d8733cf39b2ef59d17ab0d4a38d",
+    "url": "/Portfolio-2/static/media/Project-Square-AddressBook.621f8d87.jpg"
+  },
+  {
+    "revision": "546af9a09c2e4d86c4938456925da94a",
+    "url": "/Portfolio-2/static/media/Project-Square-Calc.546af9a0.jpg"
+  },
+  {
+    "revision": "1bf982e4f7fcce096cb01652cf0eb571",
+    "url": "/Portfolio-2/static/media/Project-Square-DiceGame.1bf982e4.jpg"
+  },
+  {
+    "revision": "85886219b618b9ef0815cbb92bb8c482",
+    "url": "/Portfolio-2/static/media/Project-Square-DrumKit.85886219.jpg"
+  },
+  {
+    "revision": "f12a23e2b0e35ffed41b0edf498ec6ac",
+    "url": "/Portfolio-2/static/media/Project-Square-Keycode.f12a23e2.jpg"
+  },
+  {
+    "revision": "98b23a29e6a58b59b6b51c3d4e5ed19b",
+    "url": "/Portfolio-2/static/media/Project-Square-Poke.98b23a29.jpg"
+  },
+  {
+    "revision": "b57d367002a3dbbe21e1593317e29c09",
+    "url": "/Portfolio-2/static/media/Project-Square-Todo.b57d3670.jpg"
+  },
+  {
+    "revision": "9ea75bbe6f9fa43cb9729641cb901362",
+    "url": "/Portfolio-2/static/media/Project-Square-TravelApp.9ea75bbe.jpg"
+  },
+  {
+    "revision": "b45417914938a517233af7314f842d8f",
+    "url": "/Portfolio-2/static/media/Project-Square-WeatherApp.b4541791.jpg"
+  },
+  {
+    "revision": "bfae66c32506c536070c02d20e799bce",
+    "url": "/Portfolio-2/static/media/work-picture-dicegame.bfae66c3.png"
+  },
+  {
+    "revision": "012bb3e86f0b9503ed1d2a8316762898",
+    "url": "/Portfolio-2/static/media/work-picture-drumkit.012bb3e8.png"
+  },
+  {
+    "revision": "ed070336ab1a2912e4f94f6df75c849d",
+    "url": "/Portfolio-2/static/media/work-picture-keycode.ed070336.png"
+  }
+]);
