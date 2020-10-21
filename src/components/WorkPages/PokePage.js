31958abdb68c1406859398a6339e3a75928135ab
garethscott <59648675+garethscott@users.noreply.github.com>
2020-10-21 19:05:06 +0100

    further work pictures included

--- src/components/WorkPages/PokePage.js
+++ src/components/WorkPages/PokePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import WorkPage from './WorkPage';
-// import WorkPicPoke from './images/work-picture-poke.png';
+import WorkPicPoke from './images/work-picture-pokeapp.png';
 
 const PokePage = () => {
     return (
@@ -8,13 +8,13 @@ const PokePage = () => {
             <WorkPage
                 backgroundColor="rgb(242, 167, 48)"
                 backImg="rgb(251, 212, 151)"
-                // workImg={WorkPicPoke}
+                workImg={WorkPicPoke}
                 heading="Pokemon App"
                 copy="The Pokemon app was team project working with git using a collaborator 
                 workflow. As a team we had to fix bugs and make additions to the existing Pokemon 
                 app. The Pokemon app allowed the user to search and research their favourite 
                 Pokemon."
-                workUrl="#"
+                workUrl="https://garethscott.github.io/Pokemon-App/"
                 previousUrl="/weatherapp"
                 nextUrl="/todoapp"
             />
--- src/components/WorkPages/TravelAppPage.js
+++ src/components/WorkPages/TravelAppPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import WorkPage from './WorkPage';
-// import WorkPicTravelApp from './images/work-picture-travelapp.png';
+import WorkPicTravelApp from './images/work-picture-travelapp.png';
 
 const TravelAppPage = () => {
     return (
@@ -8,13 +8,13 @@ const TravelAppPage = () => {
             <WorkPage
                 backgroundColor="rgb(5, 205, 229)"
                 backImg="rgb(149, 242, 253)"
-                // workImg={WorkPicTravelApp}
+                workImg={WorkPicTravelApp}
                 heading="Travel App"
                 copy="A full-stacked application that allows the user to 
                 discover their ideal destination based on their holiday requirements. 
                 A group project, we adopted a contributor workflow. The app was built 
                 using MongoDB, React, Express & Node.js."
-                workUrl="#"
+                workUrl="https://thevacationdetective.herokuapp.com/"
                 previousUrl="/keycode"
                 nextUrl="/weatherapp"
             />
